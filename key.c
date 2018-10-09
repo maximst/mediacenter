c3b164f66751abe7013b3473769bcb77fac6612c
Maxim Starodubcev <maxim.starodubcev@raccoongang.com>
2018-10-09 13:12:01 +0300

    Fix working keys

--- key.c
+++ key.c
@@ -9,6 +9,6 @@ int main(int argc, char *argv[]) {
     clock_gettime(CLOCK_REALTIME, &spec);
 
     file = fopen("/var/run/keys", "w+");
-    fprintf(file, "%s\n%ld.%ld", argv[1], (long)spec.tv_sec, (long)spec.tv_nsec);
+    fprintf(file, "%s\n%ld", argv[1], spec.tv_nsec);
     fclose(file);
 }
--- keys.c
+++ keys.c
@@ -7,19 +7,10 @@ typedef int bool;
 #define FALSE 0
 
 
-float get_time() {
-    struct timespec spec;
-    float cur_time = 0.0;
-    clock_gettime(CLOCK_REALTIME, &spec);
-    cur_time = difftime(spec.tv_sec, 0) + ((float)spec.tv_nsec / 1e9);
-    return cur_time;
-}
-
-
 void mainloop() {
     FILE *file;
     char current_key[8], prev_key[8];
-    float key_time, current_time;
+    long key_nsec, prev_nsec;
     struct timespec spec;
     bool keydown = FALSE;
 
@@ -32,30 +23,20 @@ void mainloop() {
     while (TRUE) {
         clock_gettime(CLOCK_REALTIME, &spec);
         fscanf(file, "%s\n", current_key);
-        fscanf(file, "%f\n", &key_time);
-        printf("kt %f\n", (float)key_time);
+        fscanf(file, "%ld\n", &key_nsec);
 
-        //printf("%f\n", difftime(spec.tv_sec, 0) + ((float)spec.tv_nsec / 1e9));
-        if (keydown && ((difftime(spec.tv_sec, 0) + ((float)spec.tv_nsec / 1e9)) - key_time) >= 0.25) {
-            //system("DISPLAY=:0 xte keyup " + prev_key)
-            printf("0000000\n");
-            printf("DISPLAY=:0 xte keyup %s\n", prev_key);
-            printf("1111111\n");
+        if (keydown && (spec.tv_nsec - key_nsec) >= 25e7) {
+            system("DISPLAY=:0 xte keyup " + prev_key);
+            prev_nsec = spec.tv_nsec;
             keydown = FALSE;
-            printf("2222222\n");
-            printf(" 1 %f %f\n", (difftime(spec.tv_sec, 0) + ((float)spec.tv_nsec / 1e9)), key_time);
         }
 
-        if (strcmp(current_key, prev_key) != 0 || (!keydown && ((difftime(spec.tv_sec, 0) + ((float)spec.tv_nsec / 1e9)) - key_time) < 0.25)) {
-            //system("DISPLAY=:0 xte keyup " + prev_key)
-            printf("DISPLAY=:0 xte keyup %s\n", prev_key);
-            //system("DISPLAY=:0 xte keydown " + current_key)
-            printf("DISPLAY=:0 xte keydown %s\n", current_key);
-            printf(" 2 %f %f\n", (difftime(spec.tv_sec, 0) + ((float)spec.tv_nsec / 1e9)), key_time);
+        if (strcmp(current_key, prev_key) != 0 || (!keydown && prev_nsec != key_nsec)) {
+            system("DISPLAY=:0 xte keyup " + prev_key)
+            system("DISPLAY=:0 xte keydown " + current_key)
             keydown = TRUE;
         }
 
-        //prev_key = current_key;
         strncpy(prev_key, current_key, 8);
 
         fseek(file, 0, SEEK_SET);
@@ -65,14 +46,9 @@ void mainloop() {
 
 
 int main(int argc, char *argv[]) {
-  mainloop();
-  exit(0);
   if (argc > 1 && argv[1] == "-n") {
-      printf("AAAAAAAAa");
       mainloop();
-      printf("BBBBBBBBBBB");
       exit(0);
-      printf("CCCCCCCCC");
   }
   int pid = fork();
   switch(pid) {
